63bb137d61d30b0e1725973bc64bd9ea7e8eae58
Necanthrope <necanthrope@gmail.com>
2015-04-24 17:37:43 -0700

    Not sure what's happening here

---
+++ src/main/java/reviewbot/entity/Formats.java
@@ -0,0 +1,69 @@
+package reviewbot.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by jtidwell on 4/24/2015.
+ */
+@Entity
+@Table(name = "formats")
+public class Formats {
+
+    @Id
+    @Column(name = "format")
+    private int format;
+
+    @Basic
+    @Column(name = "name")
+    private String name;
+
+    @Basic
+    @Column(name = "description", columnDefinition="text")
+    private String description;
+
+    public int getFormat() {
+        return format;
+    }
+
+    public void setFormat(int format) {
+        this.format = format;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Formats that = (Formats) o;
+
+        if (format != that.format) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = format;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}
